cb896f47f4f8e7e1e9830feed59a132c68334e78
Gregor Steiner <gregorsteiner7@gmail.com>
2025-03-06 15:46:41 +0000

    Update simulation.jl

--- simulation.jl
+++ simulation.jl
@@ -1,8 +1,10 @@
 
 
 using Turing, DataFrames
-using Turing: Variational
 using CairoMakie
+
+using Pkg; Pkg.activate("../gIVBMA")
+using gIVBMA
 
 include("competing_methods.jl")
 
@@ -24,33 +26,35 @@ end
 loss(y, X, Z, α, β) = 1/2 * (y - X*β - Z*α)' * Z * inv(Z'Z) * Z' * (y - X*β - Z*α)
 
 @model function IVdemo(y, X, Z)
-    σ ~ Exponential(1)
-    β ~ MvNormal(zeros(size(X, 2)), σ * I)
+    β ~ MvNormal(zeros(size(X, 2)), I)
+
     λ ~ Exponential(1)
     α = zeros(size(Z, 2))
     for j in eachindex(α)
-        α[j] ~ Laplace(0, λ)
+        α[j] ~ Laplace(0, 1 / λ)
     end
 
     Turing.@addlogprob! -loss(y, X, Z, α, β)
 end
 
 
-y, x, Z = gen_data(5000, 1, 5, 1, 1/2)
+y, x, Z = gen_data(1000, 1, 5, 2, 1/2)
 X = [ones(length(x)) x]
 
-m = IVdemo(y, X, Z)
-
-# MCMC
-chain = sample(m, NUTS(), 1000)
+chain = sample(IVdemo(y, X, Z), NUTS(), 1000)
 df = DataFrame(chain)
 
+res_givbma = givbma(y, x, Z; g_prior = "hyper-g/n")
+
+
+cols = Makie.wong_colors()
 
 fig = Figure()
 ax = Axis(fig[1, 1])
-density!(ax, df."β[2]", label = "Post Bayes sisVIVE (NUTS)")
-vlines!(ax, [1], label = "True value", color = :red)
-vlines!(ax, tsls(y, X, Z).β_hat[2], label = "TSLS", color = :green)
+density!(ax, df."β[2]", label = "Post Bayes sisVIVE")
+lines!(ax, rbw(res_givbma)[1], label = "gIVBMA", color = cols[2])
+vlines!(ax, [1], label = "True value", color = cols[3])
+vlines!(ax, tsls(y, X, Z).β_hat[2], label = "TSLS", color = cols[4])
 fig[1, 2] = Legend(fig, ax, "", framevisible = false)
 fig
 
