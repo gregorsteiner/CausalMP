7fb840cd95239cfdf7e90fe192c56120f44dc5b3
Gregor Steiner <gregorsteiner7@gmail.com>
2025-03-17 13:58:25 +0000

    Implemented GP

--- simulation.jl
+++ simulation.jl
@@ -5,12 +5,10 @@ using CairoMakie
 
 include("competing_methods.jl")
 include("PostBayesTSLS.jl")
-
-using Pkg; Pkg.activate("../gIVBMA")
-using gIVBMA
+include("IVGP.jl")
 
 
-f_x(xx) = sin(xx)
+f_x(xx) = 1.0 + xx
 f_y(yy) = cos(4*yy) + 1/3 * yy - 1/8 * yy^2 #1/10 + 1/4 * yy - 1/2 * yy^2 + - 1/5 * yy^3 + 1/3 * yy^4 - 1/25 * yy^5
 
 function gen_data(n, f_x, f_y; τ = 1, c = 1/2)
@@ -23,25 +21,18 @@ function gen_data(n, f_x, f_y; τ = 1, c = 1/2)
     return (y=y, x=x, z=z)
 end
 
-n = 10000
+n = 50
 y, x, z = gen_data(n, f_x, f_y)
 
-f_basis(x) = [x^i for i in 0:5]
-X = reduce(hcat, f_basis.(x))'
-Z = reduce(hcat, f_basis.(z))'
-
-Σ_0 = inv(X' * Z * inv(Z'Z) * Z' * X)
-res = PostBayesTSLS(y, X, Z; ω = 1/2, Σ = Σ_0)
-
-f_estimated(x, β) = f_basis(x)' * β
-
-
-xx = minimum(x):0.001:maximum(x)
 fig = Figure()
-ax = Axis(fig[1, 1])
-scatter!(ax, x, y)
-lines!(ax, xx, f_y.(xx), color = :red)
-lines!(ax, xx, f_estimated.(xx, Ref(mean(res))), color = :green)
+ax = Axis(fig[1,1])
+xx = minimum(x):0.05:maximum(x)
+fits = map(x_star -> ivgp(y, x, [ones(n) z], [x_star]; ω = 1, l = 0.1, σ2 = 1), xx)
+
+scatter!(ax, x, y, label = "Observations")
+lines!(ax, xx, f_y.(xx), label = "True Counterfactual", color = :red)
+lines!(ax, xx, map(mean, fits), color = :green, label = "IVGP Prediction")
+band!(ax, xx, map(fit -> quantile(fit, 0.025), fits), map(fit -> quantile(fit, 0.975), fits), color = (:green, 0.3))
+axislegend(ax)
+
 fig
-
-
