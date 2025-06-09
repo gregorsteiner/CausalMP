e5a6e3380b2c2be4dea14ad66962568bc54f2630
Gregor Steiner <gregorsteiner7@gmail.com>
2025-06-09 11:40:10 +0100

    Refactored code

--- SimulatedExample.R
+++ SimulatedExample.R
@@ -29,27 +29,20 @@ gen_data = function(n = 50, rho = 1/2){
 
 # Run analysis
 set.seed(13)
-d = gen_data(n = 50)
+d = gen_data(n = 500)
 
 # tsls(d[, 1], d[, 2], d[, 3])
 # ols(d[, 1], d[,2])$coef
 
-N = 2000
-B = 200
+N = 5000
+B = 500
 
 res_naive = martingale_posterior(d[, 1], d[, 2], B = B, N = N, type = "LM")
-res_gmm = martingale_posterior_gmm(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "LM")
-
-
-microbenchmark::microbenchmark(
-  Naive = martingale_posterior(d[, 1], d[, 2], B = B, N = N, type = "LM"),
-  GMM = martingale_posterior_gmm(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "LM"),
-  times = 5L
-)
+res_gmm = martingale_posterior(d[, 1], d[, 2], z = d[, 3], B = B, N = N, type = "LM")
 
 
 # compare with regular Bayesian IV
-res_rossi = rivGibbs(
+res_rossi = bayesm::rivGibbs(
   list(y = d[, 1], x = d[, 2], w = matrix(rep(1, nrow(d)), ncol = 1), z = cbind(rep(1, nrow(d)), d[ , 3])),
   Mcmc = list(R = B, nprint = 0),
 )
@@ -68,7 +61,7 @@ df_plot <- rbind(
 p = ggplot(df_plot, aes(x = beta, fill = method, color = method)) +
   geom_density(alpha = 0.4, adjust = 1.5) +
   geom_vline(xintercept = 1, linetype = "dashed", color = "black", linewidth = 0.5) +
-  annotate("text", x = 1, y = 0.1, label = "True β = 1", vjust = -0.5, hjust = 1.1, size = 4) +
+  annotate("text", x = 1, y = 0.05, label = "True β = 1", vjust = -0.5, hjust = 1.1, size = 4) +
   theme_minimal() +
   labs(
     title = "",
@@ -81,7 +74,7 @@ p = ggplot(df_plot, aes(x = beta, fill = method, color = method)) +
     text = element_text(size = 14),
     plot.title = element_text(hjust = 0.5)
   ) +
-  coord_cartesian(xlim = c(-2, 3))
+  coord_cartesian(xlim = c(-1.5, 2.8))
 p
 
 ggsave("mp_example_posterior.pdf", plot = p, width = 8, height = 4)
