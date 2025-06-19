98e2e5736e7d9abf2b9c886f86dfac63382c040d
Gregor Steiner <gregorsteiner7@gmail.com>
2025-06-19 09:44:04 +0100

    Compared DDP and LM

--- SimulatedExample.R
+++ SimulatedExample.R
@@ -32,52 +32,13 @@ gen_data = function(n = 50, rho = 1/2){
 set.seed(13)
 d = gen_data(n = 500)
 
-# tsls(d[, 1], d[, 2], d[, 3])
-# ols(d[, 1], d[,2])$coef
-
-
-y = d[, 1]
-W = d[, 2:3]
-prior <- list(strength = 1, discount = 0)
-
-grid_y <- seq(-7, 7, length.out = 100)
-grid_x <- rbind(
-  c(0, 0), c(1, 0), c(0, 1), c(1, 1)
-)
-mcmc <- list(niter = 2000, nburn = 1000)
-output <- list(grid_x = grid_x, grid_y = grid_y, out_type = "FULL", out_param = TRUE)
-
-res = PYregression(y = y, x = W, prior = prior, mcmc = mcmc, output = output)
-
-
 N = 5000
-X_new = bayesian_bootstrap(W, N)
-
-y_new_blr = bayes_lin_reg(y, X_new, N)
-y_new_ddp = ddp_predictive_sequence(1, N, X_new, res)
-
-df_plot = data.frame(
-  DDP = y_new_ddp,
-  BLR = y_new_blr,
-  group = ifelse(X_new[, 1] == 1, "Treatment", "Control"),
-  type = c(rep("Original Data", n), rep("Imputed Data", N-n))
-)
-
-df_plot = tidyr::pivot_longer(df_plot, 1:2, names_to = "Method")
-
-ggplot(df_plot, aes(x = value, colour = type, fill = type)) +
-  geom_density(alpha = 0.7) +
-  facet_wrap(~ Method + group) +
-  xlim(c(-20, 20)) +
-  theme_bw()
-
-
-N = 5000 # N > n
 B = 500
 
-res_naive = martingale_posterior(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "DDP", endogeneity = FALSE)
-res_gmm = martingale_posterior(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "DDP")
-
+res_naive_ddp = martingale_posterior(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "DDP", endogeneity = FALSE)
+res_gmm_ddp = martingale_posterior(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "DDP")
+res_naive_lm = martingale_posterior(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "LM", endogeneity = FALSE)
+res_gmm_lm = martingale_posterior(d[, 1], d[, 2], d[, 3], B = B, N = N, type = "LM")
 
 # compare with regular Bayesian IV
 res_rossi = bayesm::rivGibbs(
@@ -89,8 +50,10 @@ beta_rossi = as.numeric(res_rossi$betadraw)
 library(ggplot2)
 # Combine the data for plotting
 df_plot <- rbind(
-  data.frame(beta = sapply(res_naive, "[[", 2), method = "Naive Martingale Posterior"),
-  data.frame(beta = sapply(res_gmm, "[[", 2), method = "GMM Martingale Posterior"),
+  data.frame(beta = sapply(res_naive_ddp, "[[", 2), method = "Naive Martingale Posterior (DDP)"),
+  data.frame(beta = sapply(res_gmm_ddp, "[[", 2), method = "GMM Martingale Posterior (DDP)"),
+  data.frame(beta = sapply(res_naive_lm, "[[", 2), method = "Naive Martingale Posterior (LM)"),
+  data.frame(beta = sapply(res_gmm_lm, "[[", 2), method = "GMM Martingale Posterior (LM)"),
   data.frame(beta = beta_rossi, method = "Bayesian IV (Rossi)")
 )
 
@@ -112,7 +75,7 @@ p = ggplot(df_plot, aes(x = beta, fill = method, color = method)) +
     text = element_text(size = 14),
     plot.title = element_text(hjust = 0.5)
   ) +
-  coord_cartesian(xlim = c(-1.5, 2.8))
+  coord_cartesian(xlim = c(-0.8, 2.5))
 p
 
 ggsave("mp_example_posterior.pdf", plot = p, width = 8, height = 4)
