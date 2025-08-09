d409430af4b2a22a2481d3043071a1fc8f090546
Gregor Steiner <51028286+gregorsteiner@users.noreply.github.com>
2025-08-09 13:09:04 +0200

    Added regular TSLS

--- AJR_Example.jl
+++ AJR_Example.jl
@@ -13,16 +13,22 @@ y, x, z, W = (d.GDP, d.Exprop, d.logMort, Matrix(d[:, ["Latitude", "Africa", "As
 
 
 # run analysis
-N, B, num_trees = (200, 500, 5) # set the Martingale posterior parameters
+N, B, num_trees = (300, 1000, 5) # set the Martingale posterior parameters
 Random.seed!(42)
 
 mp_ddml = martingale_posterior(y, x; z = z, w = W, N = N, B = B, num_trees = num_trees)
+mp_tsls = martingale_posterior(y, [x W]; z = [z W], N = N, B = B)
+
 
 # plot results
 plt = density(
     clamp.(mp_ddml, -0.5, 2.5),
     linewidth = 2,
-    label = "MP DDML", xlabel = "Effect of institutions on output", ylabel = "Posterior Density"
+    label = "MP DDML IV", xlabel = "Effect of institutions on output", ylabel = "Posterior Density"
+)
+density!(
+    clamp.(getindex.(mp_tsls, 2), -0.5, 2.5),
+    label = "MP IV", linewidth = 2
 )
 xlims!(0.0, 2.0)
 
