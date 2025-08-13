8af07c26340c62e627ba87a3635894b4a2ce1a6b
Gregor Steiner <51028286+gregorsteiner@users.noreply.github.com>
2025-08-13 12:26:00 +0200

    Changed MP results

--- AJR_Example.jl
+++ AJR_Example.jl
@@ -13,23 +13,39 @@ y, x, z, W = (d.GDP, d.Exprop, d.logMort, Matrix(d[:, ["Latitude", "Africa", "As
 
 
 # run analysis
-N, B, num_trees = (300, 1000, 5) # set the Martingale posterior parameters
+N, B, num_trees = (300, 100, 5) # set the Martingale posterior parameters
 Random.seed!(42)
 
 mp_ddml = martingale_posterior(y, x; z = z, w = W, N = N, B = B, num_trees = num_trees)
 mp_tsls = martingale_posterior(y, [x W]; z = [z W], N = N, B = B)
 
 
-# plot results
-plt = density(
-    clamp.(mp_ddml, -0.5, 2.5),
+# plot posteriors results
+post_plt = density(
+    clamp.(extract_mp(mp_ddml), -0.5, 2.5),
     linewidth = 2,
     label = "MP DDML IV", xlabel = "Effect of institutions on output", ylabel = "Posterior Density"
 )
 density!(
-    clamp.(getindex.(mp_tsls, 2), -0.5, 2.5),
+    clamp.(extract_mp(mp_tsls; idx = 2), -0.5, 2.5),
     label = "MP IV", linewidth = 2
 )
 xlims!(0.0, 2.0)
 
-savefig(plt, "AJR_Results.pdf")
+savefig(post_plt, "AJR_Results.pdf")
+
+# jellyfish plot
+cols = palette(:default)
+n = length(y)
+
+data_ddml = vcat(mp_ddml...)'
+plot_ddml = plot(n:N, data_ddml[n:N, :], color = cols[1], alpha=0.2, label = false)
+plot!(n:N, data_ddml[n:N, 1], color = cols[1], alpha=0.2, label = "MP DDML IV")
+
+data_tsls = vcat(map(x -> x[2:2, :], mp_tsls)...)'
+plot_tsls = plot(n:N, data_tsls[n:N, :], color = cols[2], alpha=0.2, label = false)
+plot!(n:N, data_tsls[n:N, 1], color = cols[2], alpha=0.2, label = "MP TSLS IV")
+
+
+plot(plot_ddml, plot_tsls, ylabel = "Effect of institutions on output")
+ylims!(-3.0, 5.0)
